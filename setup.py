3b0993f93651d90bf9dfca83d7452b4e23297533
Benjamin Lowe <benjamin@sc0ville.com>
2021-01-02 13:02:34 +0900

    refactor(setup.py): add author name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 setuptools.setup(name="array_benchmark",
                  version="X",
                  description="Benchmarking sharing numpy arrays",
-                 author="Scoville",
+                 author="Benjamin Lowe",
                  python_requires=">=3.7",
                  packages=setuptools.find_packages(),
                 )
